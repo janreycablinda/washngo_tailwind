d60a652854f6ada04dac2d02f9e655e8b5fa50c6
KhalilMinalang <minalang.khalil@gmail.com>
2023-01-23 22:54:10 +0800

    added production link

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
     production: false,
-    backend_url: 'http://127.0.0.1:8000'
+    backend_url: 'http://127.0.0.1:8000',
+    // backend_url: 'https://backend.washngoservices.com',
 };
 
 /*
